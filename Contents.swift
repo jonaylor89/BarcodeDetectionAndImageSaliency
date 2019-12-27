c99b3d96328ae1f0c145e3c05b64839dcc155a59
John Naylor <jonaylor89@gmail.com>
2019-12-27 00:00:03 -0500

    Saliency and new tests

--- Contents.swift
+++ Contents.swift
@@ -45,7 +45,62 @@ extension UIImage {
         request.queueFor(image: self) { result in
             completion(result as? [VNBarcodeObservation] ?? [])
         }
+    }
+}
+
+extension UIImage {
+    
+    enum SaliencyType {
+        case objectnessBased, attentionBased
         
+        var request: VNRequest {
+            switch self {
+            case .objectnessBased:
+                return VNGenerateObjectnessBasedSaliencyImageRequest()
+            case .attentionBased:
+                return VNGenerateAttentionBasedSaliencyImageRequest()
+            }
+        }
+    }
+    
+    func detectSalientRegions(prioritising saliencyType: SaliencyType = .attentionBased, completion: @escaping (VNSaliencyImageObservation?) -> ()) {
+        
+        let request = saliencyType.request
+        
+        request.queueFor(image: self) { result in
+            completion(result?.first as? VNSaliencyImageObservation)
+        }
+    }
+    
+    func cropped(with saliencyObservation: VNSaliencyImageObservation?, to size: CGSize? = nil) -> UIImage? {
+        guard let saliencyMap = saliencyObservation, let salientObjects = saliencyMap.salientObjects else {
+            return nil
+        }
+        
+        let salientRects = salientObjects.reduce(into: CGRect.zero) {
+            rect, object in
+            rect = rect.union(object.boundingBox)
+        }
+        
+        let normalizedSalientRect = VNImageRectForNormalizedRect(salientRects, Int(self.width), Int(self.height))
+        
+        var finalImage: UIImage?
+        
+        if let desiredSize = size {
+            if self.width < desiredSize.width || self.height < desiredSize.height { return nil }
+            
+            let scaleFactor = desiredSize.scaleFactor(to: normalizedSalientRect.size)
+            
+            finalImage = self.cropped(to: normalizedSalientRect)
+            
+            finalImage = finalImage?.scaled(by: -scaleFactor)
+            
+            finalImage = finalImage?.cropped(to: desiredSize)
+        } else {
+            finalImage = self.cropped(to: normalizedSalientRect)
+        }
+        
+        return finalImage
     }
 }
 
@@ -54,4 +109,31 @@ barcodeTestImage.detectBarcodes { barcodes in
     for barcode in barcodes {
         print("Barcode data: \(barcode.payloadStringValue ?? "None")")
     }
+}
+
+let saliencyTestImage = UIImage(named: "test.jpg")!
+let thumbnailSize = CGSize(width: 80, height: 80)
+
+var attentionCrop: UIImage?
+var objectsCrop: UIImage?
+
+saliencyTestImage.detectSalientRegions(prioritising: .attentionBased) { result in
+    
+    if result == nil {
+        print("The entire image was found equally interesting")
+    }
+    
+    attentionCrop = saliencyTestImage.cropped(with: result, to: thumbnailSize)
+    print("Image was \(saliencyTestImage.width) * \(saliencyTestImage.height), now \(attentionCrop?.width ?? 0) * \(attentionCrop?.height ?? 0)")
+}
+
+saliencyTestImage.detectSalientRegions(prioritising: .objectnessBased) { result in
+    
+    if result == nil {
+        print("The entire image was found equally interesting")
+    }
+    
+    objectsCrop = saliencyTestImage.cropped(with: result, to: thumbnailSize)
+    print("Image was \(saliencyTestImage.width) * \(saliencyTestImage.height), now \(objectsCrop?.width ?? 0) * \(objectsCrop?.height ?? 0)")
+    
 }
